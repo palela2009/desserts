5a5319ab2dfafaf379f94406d930bcdf47cca29b
palela2009 <alexandre26062009@gmail.com>
2025-01-09 19:10:57 +0400

    მოვრჩი

--- src/ModalContent.jsx
+++ src/ModalContent.jsx
@@ -1,4 +1,5 @@
-import { OrderConfirmed } from './Icons.jsx';
+import React from "react";
+import { OrderConfirmed } from "./Icons.jsx";
 import dessert from "./data.json";
 
 const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts/";
@@ -7,44 +8,55 @@ export default function ModalContent({ cart, onClose }) {
   const handleStartNewOrder = () => {
     window.location.reload();
   };
-
+{
+  // თო დავაჭერთ start new order  ს მაშინ ვებსაიტი დარესტარდება 
+}
   const calculateCartTotal = () => {
     return Object.keys(cart).reduce((total, key) => {
       return total + cart[key].quantity * cart[key].price;
     }, 0);
   };
+  {
+    // აქ ვაჯამებთ
+  }
 
   return (
     <div className="modal">
       <OrderConfirmed />
       <div className="orderconfirmed">Order Confirmed</div>
       <div className="orderconfirmed1">We hope you enjoy your food!</div>
-
-      <div className="order-summary">
+    <div className="order-summary">
         <ul className="order-list">
           {Object.keys(cart).map((key) => (
-            <h4 key={key} className="order-item">
+            <li key={key} className="order-item">
               <div className="item-details">
                 <img
                   src={`${BASE_URL}${cart[key].images.thumbnail}`}
                   alt={cart[key].name}
                   className="item-thumbnail"
                 />
-                <div className='item'>
-                &nbsp; &nbsp;{cart[key].name}  <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {cart[key].quantity}  &nbsp; &nbsp; &nbsp; x   &nbsp; &nbsp; &nbsp;${cart[key].price}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <div className='item1'> $
-                {cart[key].quantity * cart[key].price}</div></div>
+                <div className="order-confirmed7">
+                  {cart[key].name}
+                  <br />
+                  {cart[key].quantity} x ${cart[key].price}
+                </div>
+                <div className="order-confirmed6">
+                  ${cart[key].quantity * cart[key].price}
+                </div>
               </div>
-            </h4>
+            </li>
           ))}
         </ul>
         <div className="order-total">
-          <h1> Order Total: ${calculateCartTotal()}</h1>
+          Order Total: ${calculateCartTotal()}
         </div>
       </div>
 
       <button onClick={handleStartNewOrder} className="orderconfirmed3">
-        <div className="orderconfirmed4">Start New Order</div>
+        Start New Order
       </button>
     </div>
   );
 }
+{//მაპით დავწერე თუ რა პროდუქტები იყიდეს და რამდენი დაჯდება ჯამში. მერე ღილაკი რომ თავიდან დაიწყოს.
+  }
